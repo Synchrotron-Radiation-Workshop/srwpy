920ef67a8bfaea725b251cbd61554f1492626590
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-11-07 05:19:15 -0500

    Adding the generated .so file to the distribution

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
             # When adding files here, remember to update MANIFEST.in as well,
             # or else they will not be included in the distribution on PyPI!
             # 'path/to/data_file',
+            '*.so'
             ]
         },
     install_requires=requirements,
