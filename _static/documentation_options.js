b138090d06efe4a566a48a465c50c64338848eed
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-11-07 15:23:17 +0000

    Update docs after building Travis build 16 of mrakitin/srwpy
    The docs were built from the branch 'master' against the commit
    1efc1c490c49ceb940f4093b3e88f269519a17ca.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/mrakitin/srwpy/jobs/451937183.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1rc4',
+    VERSION: '0.0.1rc4.post1+g1efc1c4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
