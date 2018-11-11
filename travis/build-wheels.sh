178aa9199798138fec4a6cde18c76091bcf98f36
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-11-11 03:46:55 -0500

    Get back the dependencies

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -9,17 +9,17 @@ make all
 popd
 
 # Compile wheels
-for PYBIN in /opt/python/cp36-cp36m/bin; do
+for PYBIN in /opt/python/*/bin; do
     # "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in wheelhouse/srwpy*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
 # Install packages and test
-for PYBIN in /opt/python/cp36-cp36m/bin/; do
-    "${PYBIN}/pip" install srwpy --no-index -f /io/wheelhouse
-done
+# for PYBIN in /opt/python/cp36-cp36m/bin/; do
+#     "${PYBIN}/pip" install srwpy --no-index -f /io/wheelhouse
+# done
