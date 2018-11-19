ea5f7f50592999ec9b239dd5f6d9ce5760008acd
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-11-19 06:19:03 +0000

    Update docs after building Travis build 137 of srwpy/srwpy
    The docs were built from the branch 'master' against the commit
    d7b897f88306659db8f3634aab480a9365844ed7.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/srwpy/srwpy/jobs/456823821.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1rc5.post1+g4e8f2fa',
+    VERSION: '0.0.2a5.post31+gd7b897f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
